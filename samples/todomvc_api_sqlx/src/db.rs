2c3f24cb2789a6638dc97c14e678219806c1ee5a
Stuart Harris <stuart.harris@red-badger.com>
2020-06-10 17:49:50 +0100

    use sql files instead of literals

--- samples/todomvc_api_sqlx/src/db.rs
+++ samples/todomvc_api_sqlx/src/db.rs
@@ -11,109 +11,48 @@ pub struct Todo {
 
 impl Todo {
     pub async fn find_all(pool: &PgPool) -> Result<Vec<Todo>> {
-        let records = sqlx::query_as!(
-            Todo,
-            r#"
-                SELECT id,
-                    title,
-                    completed,
-                    item_order as order
-                FROM todos
-                ORDER BY id
-            "#,
-        )
-        .fetch_all(pool)
-        .await?;
+        let records = sqlx::query_file_as!(Todo, "sql/find_all.sql",)
+            .fetch_all(pool)
+            .await?;
+
         Ok(records)
     }
 
     pub async fn find_by_id(id: i32, pool: &PgPool) -> Result<Todo> {
-        let record = sqlx::query_as!(
-            Todo,
-            r#"
-                SELECT id,
-                    title,
-                    completed,
-                    item_order as order
-                FROM todos
-                WHERE id = $1        
-            "#,
-            id,
-        )
-        .fetch_one(pool)
-        .await?;
+        let todo = sqlx::query_file_as!(Todo, "sql/find_by_id.sql", id,)
+            .fetch_one(pool)
+            .await?;
 
-        Ok(record)
+        Ok(todo)
     }
 
     pub async fn create(title: String, order: Option<i32>, pool: &PgPool) -> Result<Todo> {
-        let mut tx = pool.begin().await?;
-        let todo = sqlx::query_as!(
-            Todo,
-            r#"
-                INSERT INTO todos (title, item_order)
-                VALUES ($1, $2)
-                RETURNING id,
-                    title,
-                    completed,
-                    item_order AS order
-            "#,
-            &title,
-            order,
-        )
-        .fetch_one(&mut tx)
-        .await?;
+        let todo = sqlx::query_file_as!(Todo, "sql/create.sql", title, order,)
+            .fetch_one(pool)
+            .await?;
 
-        tx.commit().await?;
         Ok(todo)
     }
 
     pub async fn update(
         id: i32,
-        title: String,
-        completed: bool,
+        title: Option<String>,
+        completed: Option<bool>,
         order: Option<i32>,
         pool: &PgPool,
     ) -> Result<Todo> {
-        let mut tx = pool.begin().await.unwrap();
-        let todo = sqlx::query_as!(
-            Todo,
-            r#"
-                UPDATE todos
-                SET title = $1,
-                    completed = $2,
-                    item_order = $3
-                WHERE id = $4
-                RETURNING id,
-                    title,
-                    completed,
-                    item_order AS order
-            "#,
-            &title,
-            completed,
-            order,
-            id
-        )
-        .fetch_one(&mut tx)
-        .await?;
+        let todo = sqlx::query_file_as!(Todo, "sql/update.sql", title, completed, order, id)
+            .fetch_one(pool)
+            .await?;
 
-        tx.commit().await.unwrap();
         Ok(todo)
     }
 
     pub async fn delete(id: i32, pool: &PgPool) -> Result<u64> {
-        let mut tx = pool.begin().await?;
-        let deleted = sqlx::query!(
-            r#"
-                DELETE FROM todos
-                WHERE id = $1
-            "#,
-            id
-        )
-        .execute(&mut tx)
-        .await?;
+        let deleted = sqlx::query_file!("sql/delete.sql", id)
+            .execute(pool)
+            .await?;
 
-        tx.commit().await?;
         Ok(deleted)
     }
 }
