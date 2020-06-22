f7898c148aaafdb3266a2fb6d11592d21e230125
Stuart Harris <stuart.harris@red-badger.com>
2020-06-22 15:30:51 +0100

    better error handling

--- samples/todomvc/src/auth.rs
+++ samples/todomvc/src/auth.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 // use jsonwebtoken::{decode, DecodingKey, Validation};
 use base64::decode;
 use serde::{Deserialize, Serialize};
@@ -63,7 +63,7 @@ fn decode_jwt(token: &str) -> Result<Claims> {
     // let token = decode::<Claims>(&token, &key, &Validation::default()).unwrap();
     // todo!()
     let parts: Vec<&str> = token.split('.').collect();
-    let claims = parts.get(1).expect("malformed token");
+    let claims = parts.get(1).ok_or_else(|| anyhow!("malformed token"))?;
     let claims = decode(claims)?;
     let claims = str::from_utf8(&claims[..])?;
     let claims = serde_json::from_str(claims)?;
